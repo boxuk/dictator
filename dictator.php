57866c9556d8551dc67aa7ed89aded6f99949644
Daniel Bachhuber <d@danielbachhuber.com>
2013-06-10 14:12:57 -0700

    Initial commit

---
+++ dictator.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Dictator
+Version: 0.1-alpha
+Description: The Dictator controls the State of WordPress
+Author: danielbachhuber, humanmade
+Author URI: http://hmn.md/
+Plugin URI: http://wordpress.org/extend/plugins/dictator/
+Text Domain: dictator
+Domain Path: /languages
+*/
