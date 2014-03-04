4af6e2988faa60aa45380628b415c8765c601bd4
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-03 18:51:12 -0800

    A basic dictator file with an autoloader

---
+++ dictator.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The Dictator controls the State of WordPress with WP-CLI
+ * 
+ * Use wisely.
+ */
+
+if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
+	return;
+}
+
+define( 'DICTATOR', true );
+
+/**
+ * Auto-load Dictator files
+ */
+function dictator_autoloader( $class ) {
+
+	if ( $class[0] === '\\') {
+		$class = substr( $class, 1 );
+	}
+
+	if ( 0 !== strpos( $class, 'Dictator' ) ) {
+		return;
+	}
+
+	// Turn Dictator\States\State into ./php/states/class-state.php
+	$file_parts = explode( '\\', str_replace( '_', '-', strtolower( $class ) ) );
+	array_shift( $file_parts );
+	$file_name = array_pop( $file_parts );
+	$file_name = 'class-' . $file_name . '.php';
+
+	$file_path = dirname( __FILE__ ) . '/php/' . implode( '/', $file_parts ) . '/' . $file_name;
+	if ( is_file( $file_path ) ) {
+		require $file_path;
+	}
+
+}
+spl_autoload_register( 'dictator_autoloader' );
+
