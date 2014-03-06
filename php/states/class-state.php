e4e06dc4747b73674b85daa500ea6a8a4b706c94
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-06 11:14:35 -0800

    A state controls regions of WordPress

--- php/states/class-state.php
+++ php/states/class-state.php
@@ -1,14 +1,46 @@
 <?php
 
-namespace Dictator;
+namespace Dictator\States;
 
 /**
- * A state file describes the schema for a certain aspect of WordPress
+ * A state controls certain regions of WordPress
  */
-
 abstract class State {
 
+	/**
+	 * Data included in the Yaml file
+	 */
+	protected $yaml;
 
+	/**
+	 * Components of WordPress controlled by this state
+	 */
+	protected $regions = array();
+
+	public function __construct( $yaml ) {
+
+		$this->yaml = $yaml;
+
+	}
+
+	/**
+	 * Get the regions associated with this state
+	 * 
+	 * @return array
+	 */
+	public function get_regions() {
+
+		$regions = array();
+		foreach( $this->regions as $key => $class ) {
+
+			$data = ( ! empty( $this->yaml[ $key ] ) ) ? $this->yaml[ $key ] : array();
+
+			$regions[] = new $class( $data );
+
+		}
+		return $regions;
+
+	}
 
 	
 }
