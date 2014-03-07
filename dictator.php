b18bfdce3bd47dd746342a827c14ab5e491ee28f
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-06 16:59:20 -0800

    We don't need `array_column()`, as it turns out

--- dictator.php
+++ dictator.php
@@ -15,10 +15,6 @@ define( 'DICTATOR', true );
  * Some files need to be manually loaded
  */
 require_once dirname( __FILE__ ) . '/autoload.php';
-// array_column was introduced in PHP 5.5
-if ( ! function_exists( 'array_column' ) ) {
-	require_once dirname( __FILE__ ) . '/lib/ramsey/array_column/src/array_column.php';
-}
 require_once dirname( __FILE__ ) . '/php/class-dictator.php';
 require_once dirname( __FILE__ ) . '/php/class-dictator-cli-command.php';
 
