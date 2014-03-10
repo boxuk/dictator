fadf1483018564f64bd6d53b5a6af1839b85ade9
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-10 16:50:27 -0700

    Support for imposing terms on a site

--- php/regions/class-terms.php
+++ php/regions/class-terms.php
@@ -7,6 +7,9 @@ class Terms extends Region {
 	private $terms;
 
 	private $fields = array(
+		'name'         => 'name',
+		'parent'       => 'parent',
+		'description'  => 'description',
 		);
 
 	/**
@@ -20,7 +23,66 @@ class Terms extends Region {
 	 */
 	public function impose( $key, $value ) {
 
-		// @todo 
+		if ( ! taxonomy_exists( $key ) ) {
+			return new \WP_Error( 'invalid-taxonomy', "Invalid taxonomy" );
+		}
+
+		foreach( $value as $slug => $term_values ) {
+
+			$term = get_term_by( 'slug', $slug, $key );
+			if ( ! $term ) {
+
+				$ret = wp_insert_term( $slug, $key );
+				if ( is_wp_error( $ret ) ) {
+					return $ret;
+				}
+				$term = get_term_by( 'id', $ret['term_id'], $key );
+			}
+
+			foreach( $this->fields as $yml_field => $model_field ) {
+
+				if ( ! isset( $term_values[ $yml_field ] ) ) {
+					continue;
+				}
+
+				switch ( $yml_field ) {
+					case 'name':
+					case 'description':
+
+						if ( $term_values[ $yml_field ] == $term->$model_field ) {
+							break;
+						}
+
+						wp_update_term( $term->term_id, $key, array( $model_field => $term_values[ $yml_field ] ) );
+
+						break;
+
+					case 'parent':
+
+						if ( $term_values[ $yml_field ] ) {
+							
+							$parent_term = get_term_by( 'slug', $term_values[ $yml_field ], $key );
+							if ( ! $parent_term ) {
+								return new \WP_Error( 'invalid-parent', sprintf( "Parent is invalid for term: %s", $slug ) );
+							}
+
+							if ( $parent_term->term_id == $term->parent ) {
+								break;
+							}
+
+							wp_update_term( $term->term_id, $key, array( $model_field => $parent_term->term_id ) );
+						} else if ( ! $term_values[ $yml_field ] && $term->parent ) {
+							wp_update_term( $term->term_id, $key, array( $model_field => 0 ) );
+						}
+
+						break;
+				}
+
+			}
+
+		}
+
+		return true;
 	}
 
 	/**
@@ -34,7 +96,7 @@ class Terms extends Region {
 		// Check each declared term in state data against WordPress
 		foreach( $this->get_imposed_data() as $taxonomy => $taxonomy_data ) {
 
-			$result = $this->get_term_difference( $taxonomy, $taxonomy_data );
+			$result = $this->get_taxonomy_difference( $taxonomy, $taxonomy_data );
 
 			if ( ! empty( $result ) ) {
 				$this->differences[ $taxonomy ] = $result;
@@ -94,6 +156,36 @@ class Terms extends Region {
 	public function get_imposed_data() {
 
 		return $this->data;
+
+	}
+
+	/**
+	 * Get the difference between the declared taxonomy state and
+	 * the actual taxonomy state
+	 * 
+	 * @param string $taxonomy
+	 * @param array $taxonomy_data
+	 * @return array|false 
+	 */
+	protected function get_taxonomy_difference( $taxonomy, $taxonomy_data ) {
+
+		$result = array(
+			'dictated'        => $taxonomy_data,
+			'current'         => array(),
+		);
+
+		$current_data = $this->get_current_data();
+		if ( ! isset( $current_data[ $taxonomy ] ) ) {
+			return $result;
+		}
+
+		$result['current'] = $current_data[ $taxonomy ];
+
+		if ( \Dictator::array_diff_recursive( $result['dictated'], $result['current'] ) ) {
+			return $result;
+		} else {
+			return false;
+		}
 
 	}
 
