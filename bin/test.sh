b3676789f0e36b082da91bb38caac7cb914a4481
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-28 09:53:13 -0700

    Updated testing framework

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,8 @@ set -ex
 
 # Set up the environment
 WP_CLI_DIR=${WP_CLI_DIR-/tmp/wp-cli}
+PACKAGE_TEST_CONFIG_PATH=${PACKAGE_TEST_CONFIG_PATH-/tmp/wp-cli-package-test.yml}
+export PACKAGE_TEST_CONFIG_PATH=${PACKAGE_TEST_CONFIG_PATH}
 
 # Run the functional tests
 $WP_CLI_DIR/vendor/bin/behat --config=$WP_CLI_DIR/behat.yml features
