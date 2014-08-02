802d8bcd7e8462e56b9f6de07cc3fa102816fa9c
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-02 09:38:25 -0400

    Update package install script

--- bin/install-package-tests.sh
+++ bin/install-package-tests.sh
@@ -1,48 +1,30 @@
 #!/usr/bin/env bash
 
-PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
-WP_CLI_DIR=${WP_CLI_DIR-/tmp/wp-cli}
-PACKAGE_TEST_CONFIG_PATH="/tmp/wp-cli-package-test.yml"
-
 set -ex
 
-install_wp_cli_suite() {
+install_wp_cli() {
 
-	# Set up WP-CLI
-	if [ ! -d $WP_CLI_DIR ]
-		then
-			git clone --quiet -b scaffold-package-tests https://github.com/wp-cli/wp-cli.git $WP_CLI_DIR
-			cd $WP_CLI_DIR
-			curl -sS https://getcomposer.org/installer | php
-			chmod +x composer.phar
-			./composer.phar install
-	else
-		cd $WP_CLI_DIR
-		git pull
-	fi
+	# the Behat test suite will pick up the executable found in $WP_CLI_BIN_DIR
+	mkdir -p $WP_CLI_BIN_DIR
+	wget https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar
+	mv wp-cli-nightly.phar $WP_CLI_BIN_DIR/wp
+	chmod +x $WP_CLI_BIN_DIR/wp
+
 }
 
-set_package_context() {
+download_behat() {
 
-	cd $PACKAGE_DIR
-
-	touch $PACKAGE_TEST_CONFIG_PATH
-	printf 'require:' > $PACKAGE_TEST_CONFIG_PATH
-	requires=$(php $WP_CLI_DIR/utils/get-package-require-from-composer.php composer.json)
-	for require in "${requires[@]}"
-	do
-		printf "$config_file\n%2s-%1s$PACKAGE_DIR/$require" >> $PACKAGE_TEST_CONFIG_PATH
-	done
+	cd ../
+	wget https://github.com/downloads/Behat/Behat/behat.phar
+	chmod +x behat.phar
 
 }
 
 install_db() {
-
-	# For wp_cli_test too
 	mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot
 	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
 }
 
-install_wp_cli_suite
-set_package_context
+install_wp_cli
+download_behat
 install_db
